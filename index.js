d577c1d2faee81819e914d28349bd749303af894
Dexter Lohnes <dexterlohnes@gmail.com>
2018-01-03 16:50:58 -0800

    +Starting to get things set up :)

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/// Includes
+const express = require('express');
+const bodyParser = require('body-parser');
+const Promise = require('bluebird');
+const redis = require('redis');
+Promise.promisifyAll(redis);
+const app = express();
+const db = require('./TintsDBConnector.js');
+const tints = require('./tintsMessages.js');
+
+/// Flags / constants
+const IN_MAINTENANCE_MODE = false;
+
+/// Set up exress app
+app.set('port', (process.env.PORT || 5000));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot');
+});
+
+app.post('/tip/', function (req, res) {
+    console.log('someone sent a tip!')
+    console.log(JSON.stringify(req));
+    // routeMessagingEvents(req);
+    res.sendStatus(200);
+});
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'));
+});
+
